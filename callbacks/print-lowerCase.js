7128fd6f8c9ecd59fac83e62f94c106855c8084c
Keishon Ross <keishonross97@gmail.com>
2018-11-04 23:45:38 +0000

    setup templates for new files with proper paths

--- callbacks/print-lowerCase.js
+++ callbacks/print-lowerCase.js
@@ -1,2 +1,14 @@
 const getHTML = require('../http-functions');
 
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/lowercase.html'
+  };
+  
+  function printLowerCase (html) {
+  
+    /* Write your code here! */
+  
+  }
+  
+  getHTML(requestOptions, printLowerCase);
--- callbacks/print-reverse.js
+++ callbacks/print-reverse.js
@@ -1,2 +1,15 @@
 const getHTML = require('../http-functions');
 
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/reverse.html'
+  };
+  
+  function printReverse (html) {
+  
+    /* Write your code here! */
+  
+  }
+  
+  getHTML(requestOptions, printReverse);
+
