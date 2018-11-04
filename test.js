ed8f451df58f5460aace077e58a7392fb17678d2
Keishon Ross <keishonross97@gmail.com>
2018-11-04 23:38:06 +0000

    made function into a module, created test file to confirm it works

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
-const https = require('https');
+const getHTML = require('./ http-functions');
 
-function getAndPrintHTMLChunks () {
-    
-    const requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
-    };
-  
-    https.get(requestOptions, (res) => {
-        res.setEncoding('utf-8');
-
-        res.on('data', (data) => {
-            console.log("Chunk Received - .Length: ", data.length + '\n');
-        });
-        res.on('end', () =>{
-            console.log("Response Stream: Complete");
-        })
-    });
-};
-
-getAndPrintHTMLChunks();
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step5.html'
+  };
+  function printHTML (html) {
+    console.log(html);
+  };
+  getHTML(requestOptions, printHTML);
