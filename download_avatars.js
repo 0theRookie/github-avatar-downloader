3d1965b9053079d393bfd2aad5ea1cd1fbd07d00
Keishon Ross <keishonross97@gmail.com>
2018-11-05 00:59:11 +0000

    updated .gitignore and required authorization key

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,25 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
-  }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': secrets.GITHUB_TOKEN
+        }
+    };
+   
+    request(url, function(err, res, body) {
+        cb(err, body);
+      });  
+}
+
+  
+getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
   });
