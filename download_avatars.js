6787a60e6e27b697037bdd0591c7c67f75b6e8f8
Keishon Ross <keishonross97@gmail.com>
2018-11-12 19:20:32 +0000

    initialized npm

--- download_avatars.js
+++ download_avatars.js
@@ -6,13 +6,15 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
 
     var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        url: '"https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors"',
         headers: {
             'User-Agent': 'request',
             'Authorization': secrets.GITHUB_TOKEN
         }
     };
-   
+    
+    const url = JSON.parse(options.url);
+
     request(url, function(err, res, body) {
         cb(err, body);
       });  
